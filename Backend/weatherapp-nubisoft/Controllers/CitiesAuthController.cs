11a5152a3475774a51385feaf3e00c23eedfa8bf
illia <ilgut1000@gmail.com>
2025-04-14 04:53:34 +0200

    add weather-favourite-cities-forecast endpoint

--- Backend/weatherapp-nubisoft/Controllers/CitiesAuthController.cs
+++ Backend/weatherapp-nubisoft/Controllers/CitiesAuthController.cs
@@ -56,4 +56,38 @@ public class CitiesAuthController : Controller
         
         return Ok(JsonSerializer.SerializeToNode(weatherBroadcast));
     }
+
+    // default behavior : forecast for 1 day
+    [HttpGet("registered/weather-favourite-cities-forecast")]
+    public async Task<IActionResult> GetWeatherFavouriteCitiesForecast([FromQuery] int days = 1)
+    {
+        var userId = User.FindFirst("jti")?.Value;
+        
+        if (userId is null)
+            return Unauthorized("Problem with authorizing, try again.");
+        
+        var favoriteCities = await _context.FavouriteCities
+            .Where(fc => fc.AppUserId == userId)
+            .Select(fc => fc.CityName)
+            .ToListAsync();
+
+        var weatherBroadcast = new List<ForecastWeatherResponseModel>();
+        
+        foreach (var city in favoriteCities)
+        {
+            var weatherResponse = _httpClient.GetAsync(
+                $"forecast.json?key={_configuration.GetValue<string>("WeatherApiKey")}&q={city}&days={days}");
+            
+            var weatherJson = await weatherResponse.Result.Content.ReadAsStringAsync();
+            
+            if (weatherResponse.Result.StatusCode != System.Net.HttpStatusCode.OK)
+                return BadRequest("Could not retrieve weather data");
+            
+            var weatherDeserialized = JsonSerializer.Deserialize<WeatherResponseModel>(weatherJson);
+            
+            weatherBroadcast.Add(new ForecastWeatherResponseModel(weatherDeserialized, days));
+        }
+        
+        return Ok(JsonSerializer.SerializeToNode(weatherBroadcast));
+    }
 }
